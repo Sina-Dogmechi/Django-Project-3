a23b1ee80df5a8846245cd5be3a1c8fffd590c8b
Sina-Dogmechi <sinadogmechi@gmail.com>
2026-06-22 23:16:30 +0330

    new endpoint for viewing user-profile and ability to change username

--- accounts/api_views.py
+++ accounts/api_views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from .serializers import UserRegisterSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 
@@ -14,3 +15,18 @@ class UserRegisterView(APIView):
         user.set_password(serializer.validated_data['password'])
         user.save()
         return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        username = request.data.get('username')
+        request.user.username = username
+        request.user.save(update_fields=['username'])
+        return Response(UserSerializer(request.user).data)
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from rest_framework.authtoken import views as token_views
 app_name = 'accounts'
 urlpatterns = [
     path('register/', api_views.UserRegisterView.as_view()),
-    path('api-token-auth/', token_views.obtain_auth_token)
+    path('api-token-auth/', token_views.obtain_auth_token),
+    path('profile/', api_views.UserProfileView.as_view()),
 ]
