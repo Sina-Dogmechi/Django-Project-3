e0a0831f98ce0b2296dc73ce5193a9cd36c062c0
Sina-Dogmechi <sinadogmechi@gmail.com>
2026-06-22 11:14:43 +0330

    create 2 models for questions and answers apps

--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core.models import BaseModel
+from django.conf import settings
 
-# Create your models here.
+
+class Answer(BaseModel):
+    question = models.ForeignKey('questions.Question', on_delete=models.CASCADE, related_name='answers')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_answers')
+    body = models.TextField()
+    is_accepted = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('-is_accepted', '-score', '-created')
+
+    def __str__(self):
+        return f"Answer #{self.pk}"
---
+++ questions/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from core.models import BaseModel
+from django.conf import settings
+
+
+class Question(BaseModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions')
+    title = models.CharField(max_length=255, db_index=True)
+    body = models.TextField()
+    views_count = models.PositiveIntegerField(default=0)
+    answers_count = models.PositiveIntegerField(default=0)
+    score = models.IntegerField(default=0)
+    accepted_answers = models.ForeignKey('answers.Answer', blank=True, null=True, on_delete=models.SET_NULL, related_name='accepted_for_questions')
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
